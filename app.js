5d3af8e58915ecb119d59d3f724511f448dcdb22
TFuray <pk.master881@gmail.com>
2022-11-29 23:05:57 -0800

    added handlebats skeleton

--- app.js
+++ app.js
@@ -1,63 +1,39 @@
 const express = require('express')
-const app = express()
-const MethodOverride = require('method-override')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const flash = require('express-flash')
 const logger = require('morgan')
-const bodyParser = require('body-parser')
+const { engine } = require('express-handlebars')
 const moment = require('moment')
 const connectDB = require('./config/db')
 const mainRoutes = require('./routes/main')
 const ticketRoutes = require('./routes/tickets')
 
-dotenv.config({ path: './config/config.env' })
+require('dotenv').config({ path: './config/config.env' })
 
+// Passport config
+require('./config/passport')(passport)
+
+// connect MongoDB
 connectDB()
 
-// calling ticket.js from models
-const Ticket = require('./models/ticket')
+// init app
+const app = express()
 
-// set parser && global files && middleware
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use((req, res, next) => {
-  res.locals.moment = moment
-  next()
-})
+// Handlebars
+app.engine(
+  'hbs',
+  engine({
+    extname: 'hbs',
+    layoutsDir: './views/layouts/'
+  })
+)
+app.set('view engine', 'hbs')
+app.set('views', './views')
 
-//@index.js     route '/'
-app.get('/', async (req, res) => {
-  try {
-    const tickets = await Ticket.find({ status: 'Open' })
-      .sort({ createdAt: 'desc' })
-      .lean()
-    res.render('index', { tickets })
-  } catch (err) {
-    console.error(err)
-  }
-})
 
-app.get('/add-ticket', async (req, res) => {
-  try {
-    await res.render('newTicket.ejs')
-  } catch (err) {
-    console.error(err)
-  }
-})
-
-app.post('/add-ticket', async (req, res) => {
-  try {
-    await Ticket.create(req.body)
-    res.redirect('/')
-  } catch (err) {
-    console.error(err)
-  }
-})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
