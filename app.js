8385bc3cb45ab4a747a405e2e081d3952367152c
TFuray <pk.master881@gmail.com>
2022-12-14 11:24:20 -0800

    added method override

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
+const methodOverride = require('method-override')
 const flash = require('express-flash')
 const logger = require('morgan')
 const { engine } = require('express-handlebars')
@@ -50,10 +51,12 @@ app.engine(
 app.set('view engine', 'hbs')
 app.set('views', './views')
 
+// Middlewares
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 app.use(logger('dev'))
+app.use(methodOverride('_method'))
 
 // Sessions
 app.use(
